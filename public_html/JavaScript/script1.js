497b2879ac1cf7d9b89717bc6bdc86730ca323eb
Marlon <m.c4n1as@gmail.com>
2018-02-26 08:59:10 -0700

    Added CSS and Javascript with HTML
    I've created the boilerplate structure of the HTML. I've also added a javascript folder and CSS folder.

---
+++ public_html/JavaScript/script1.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+alert("script1.js");
+
+var width = 700, height = 600, pi = Math.PI;
