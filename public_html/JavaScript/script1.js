7b8bdb8022aae9ad0b96c4805f31f721e680f3b0
Marlon <m.c4n1as@gmail.com>
2018-02-26 09:23:26 -0700

    Added Javascript code
    Added many lines of code to the javascript file.  Created variables player, ai, ball and made functions to draw the playing field.

--- public_html/JavaScript/script1.js
+++ public_html/JavaScript/script1.js
@@ -8,3 +8,90 @@
 alert("script1.js");
 
 var width = 700, height = 600, pi = Math.PI;
+
+//update game state variables
+var canvas, ctx, keystate;
+
+//for logic game variables
+var player, ai, ball;
+
+
+player = {
+     x: null,
+     y: null,
+     width: 20,
+     height: 100,
+     
+     update: function(){},
+     draw: function(){
+         ctx.fillRect(this.x,this.y,this.width,this.height);
+     }
+};
+
+ai = {
+     x: null,
+     y: null,
+     width: 20,
+     height: 100,
+     
+     update: function(){},
+     draw: function(){
+         ctx.fillRect(this.x,this.y,this.width,this.height);
+     }
+};
+
+ball = {
+     x: null,
+     y: null,
+     side:20,
+     
+     update: function(){},
+     draw: function(){
+         ctx.fillRect(this.x,this.y,this.side,this.side);
+     }
+};
+
+function main(){
+    canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    ctx = canvas.getContext("2d");
+    document.body.appendChild(canvas);
+    
+    init();
+    
+    var loop = function(){
+        update();
+        draw();
+        
+        window.requestAnimationFrame(loop,canvas);
+    };
+    
+    window.requestAnimationFrame(loop,canvas);
+}
+
+//setting up the playing field and the variables in it
+function init(){
+    player.x = player.width;
+    player.y = (height-player.height)/2;
+    
+    ai.x = width-(player.width+ai.width);
+    ai.y = (height-ai.height)/2;
+    
+    ball.x = (width-ball.side)/2;
+    ball.y = (height-ball.side)/2;
+}
+
+function update(){
+    ball.update();
+    player.update();
+    ai.update();
+}
+
+function draw(){
+    ball.draw();
+    player.draw();
+    ai.draw();
+}
+
+main();
